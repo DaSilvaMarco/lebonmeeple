22064e0146451cfb4c4ed18d6155fe48ca059e8a
DaSilvaMarco <tdasilva.marco@gmail.com>
2025-08-20 23:21:54 +0200

    fix: last fix

--- apps/front-end/src/utils/api-config.ts
+++ apps/front-end/src/utils/api-config.ts
@@ -1,6 +1,6 @@
 export const getApiBaseUrl = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 'http://api.lebonmeeple.online';
+    return 'https://api.lebonmeeple.online';
   }
   if (process.env.NODE_ENV === 'development') {
     return 'http://localhost:3000';
