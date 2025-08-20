283248c1ca3efce74d3f411a47ba6415d9c15421
DaSilvaMarco <tdasilva.marco@gmail.com>
2025-08-20 19:45:02 +0200

    fix: getbaseurl

--- apps/front-end/src/utils/api-config.ts
+++ apps/front-end/src/utils/api-config.ts
@@ -1,18 +1,3 @@
 export const getApiBaseUrl = () => {
-  if (process.env.NEXT_PUBLIC_API_URL) {
-    return 'http://lebonmeeple.online/api';
-  }
-
-  if (typeof window !== 'undefined') {
-    return 'http://localhost:3000';
-  }
-
-  if (
-    process.env.NODE_ENV === 'development' &&
-    process.env.DOCKER_ENV === 'true'
-  ) {
-    return 'http://backend:3000';
-  }
-
-  return 'http://localhost:3000';
+  return 'http://lebonmeeple.online/api';
 };
